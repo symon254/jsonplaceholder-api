071615dd56b3ee723417b5ea56bebc076fffa12a
symon254 <79975499+symon254@users.noreply.github.com>
2022-02-16 15:16:16 +0300

    initialization

---
+++ src/Actions/types.js
@@ -0,0 +1,27 @@
+export const CREATE_TODO = ' CREATE_TODO' 
+export const CREATE_TODO_SUCCESS = ' CREATE_TODO_SUCCESS' 
+export const CREATE_TODO_ERROR = ' CREATE_TODO_ERROR' 
+
+
+
+export const GET_TODOS = ' GET_TODOS' 
+export const GET_TODOS_SUCCESS = ' GET_TODOS_SUCCESS' 
+export const GET_TODOS_ERROR = ' GET_TODOS_ERROR' 
+
+
+
+export const START_EDITING =' START_EDITING'
+export const CANCEL_EDITING = ' CANCEL_EDITING'
+
+export const UPDATE_TODO = ' UPDATE_TODO' 
+export const UPDATE_TODO_SUCCESS = ' UPDATE_TODO_SUCCESS' 
+export const UPDATE_TODO_ERROR = ' UPDATE_TODO_ERROR' 
+
+export const COMPLETE_TODO = 'COMPLETE_TODO'
+
+
+export const DELETE_TODO = ' DELETE_TODO' 
+export const DELETE_TODO_SUCCESS = ' DELETE_TODO_SUCCESS' 
+export const DELETE_TODO_ERROR = ' DELETE_TODO_ERROR' 
+
+
